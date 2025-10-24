cd78c3084de6e026ccccf079d95fdfb88521bd1e
Yazmin <moyanoyazminalejandra@gmail.com>
2025-10-24 16:03:01 -0300

    Add Update producto

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
 
 
     //Agregar un producto
-    string insertQuery = "INSERT INTO Productos (Descripcion, Precio) VALUES ('Teclado Aitech', 7000)";
+   /* string insertQuery = "INSERT INTO Productos (Descripcion, Precio) VALUES ('Teclado Aitech', 7000)";
     using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
     {
         insertCmd.ExecuteNonQuery();
@@ -41,7 +41,35 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
         {
             Console.WriteLine($"ID: {reader["idProducto"]}, Descripcion: {reader["Descripcion"]}, Precio: {reader["Precio"]}");
         }
+    }*/
+
+  /*  string insertQuery = "INSERT INTO Presupuestos (NombreDestinatario, FechaCreacion) VALUES ('Juan Perez', '2024-10-24')";
+    using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
+    {
+        insertCmd.ExecuteNonQuery();
+        Console.WriteLine("Dato insertado en la tabla Presupuestos");
     }
+    // Leer datos
+    string selectQuery = "SELECT * FROM Presupuestos";
+    using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
+    using (SqliteDataReader reader = selectCmd.ExecuteReader())
+    {
+        Console.WriteLine("Datos en la tabla 'presupuestos':");
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["idPresupuesto"]}, Destinatario: {reader["NombreDestinatario"]}, Fecha : {reader["FechaCreacion"]}");
+        }
+    }*/
+
+    string updateQuery = "UPDATE Productos SET Precio = 80000 WHERE idProducto = 1";
+    using (SqliteCommand updateCmd = new SqliteCommand(updateQuery, connection))
+    {
+        updateCmd.ExecuteNonQuery();
+        Console.WriteLine("Dato modificado en la tabla Productos");
+        int filasAfectadas = updateCmd.ExecuteNonQuery();
+        Console.WriteLine($"Filas modificadas: {filasAfectadas}");
+    }
+
 
     connection.Close();
 }
